64c1594784d3f13bebd86f51b3156510ede516f0
Dhruv Rawat <dhruvrawat09@gmail.com>
2020-10-17 13:46:53 -0400

    Motors move and configured powers so car moves slow enough to ensure good measurements

---
+++ motor_test.py
@@ -0,0 +1,108 @@
+import pid  # Imports PID Class
+import RPi.GPIO as GPIO
+import time
+
+# We will need to test values to find the optimum ones
+angle_pid = pid.PID(0.7, 0.2, 0.3, 200, 60, 80)  # Constructs PID class for when the robot needs to rotate
+forward_pid = pid.PID(0.4, 0.07, 0.1, 200, 40,
+                      65)  # Constructs PID class for when the robot needs to move forwards and backwards
+
+desired_angle = 0  # We want the ball to be at 0 degrees relative to the centre
+desired_distance = 10;  # DECIDE THIS
+
+# Define ports for motor 1 (Left)
+motor_1_forward = 24
+motor_1_backward = 23
+motor_1_en = 25
+
+# Define ports for motor 2 (Right)
+
+motor_2_forward = 27
+motor_2_backward = 22
+motor_2_en = 17
+
+# Initialize GPIO
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(motor_1_forward, GPIO.OUT)
+GPIO.setup(motor_1_backward, GPIO.OUT)
+GPIO.setup(motor_1_en, GPIO.OUT)
+GPIO.output(motor_1_forward, GPIO.LOW)
+GPIO.output(motor_1_backward, GPIO.LOW)
+GPIO.setup(motor_2_forward, GPIO.OUT)
+GPIO.setup(motor_2_backward, GPIO.OUT)
+GPIO.setup(motor_2_en, GPIO.OUT)
+GPIO.output(motor_2_forward, GPIO.LOW)
+GPIO.output(motor_2_backward, GPIO.LOW)
+pwm_motor_1 = GPIO.PWM(motor_1_en, 1000)
+pwm_motor_2 = GPIO.PWM(motor_2_en, 1000)
+
+# Good PWM values to move straight
+pwm_motor_1.start(75)
+pwm_motor_2.start(70)
+
+# Tara and Amanda's functions apply in next two lines
+left_or_right = 1  # 0 for Left, 1 for Right, 2 for centre
+current_angle = 40  # This value will be given by Amanda and Tara's function
+current_distance = 1  # This value will be given by Amanda and Tara's function
+
+print("Starting")
+done = False
+while not (done):
+
+    # Uses a different PID depending if the car needs to rotate or move forward/backwards to say the ball is "dead ahead"
+    motor_pwm = angle_pid.get_output(current_angle, desired_angle)
+
+    current_angle = current_angle - 1
+
+    if left_or_right == 0:  # Left
+        pwm_motor_1.ChangeDutyCycle(25)
+        pwm_motor_2.ChangeDutyCycle(motor_pwm)
+        time.sleep(0.05)
+        GPIO.output(motor_1_forward, GPIO.LOW)
+        GPIO.output(motor_1_backward, GPIO.LOW)
+        GPIO.output(motor_2_forward, GPIO.HIGH)
+        GPIO.output(motor_2_backward, GPIO.LOW)
+        # power right motor using PWM
+        # power left motor at about 5-10%
+    elif left_or_right == 1:  # Right
+        pwm_motor_1.ChangeDutyCycle(motor_pwm)
+        time.sleep(0.05)
+        GPIO.output(motor_1_forward, GPIO.HIGH)
+        GPIO.output(motor_1_backward, GPIO.LOW)
+        GPIO.output(motor_2_forward, GPIO.LOW)
+        GPIO.output(motor_2_backward, GPIO.LOW)
+        # power left motor using PWM
+        # power right motor at about 5-10%
+    elif left_or_right == 2:  # Centre
+        pwm_motor_1.ChangeDutyCycle(motor_pwm + 5)
+        pwm_motor_2.ChangeDutyCycle(motor_pwm)
+        time.sleep(0.05)
+        GPIO.output(motor_1_forward, GPIO.HIGH)
+        GPIO.output(motor_1_backward, GPIO.LOW)
+        GPIO.output(motor_2_forward, GPIO.HIGH)
+        GPIO.output(motor_2_backward, GPIO.LOW)
+        print("Exiting")
+        GPIO.cleanup()
+        print("GPIO Clean up")
+        print()
+        # power both motors using PWM
+
+    time.sleep(0.3)
+
+    GPIO.output(motor_1_forward, GPIO.LOW)
+    GPIO.output(motor_1_backward, GPIO.LOW)
+    GPIO.output(motor_2_forward, GPIO.LOW)
+    GPIO.output(motor_2_backward, GPIO.LOW)
+
+    if (current_angle == 0):
+        done = True
+
+GPIO.cleanup()
+print("GPIO Clean up")
+""" END OF INFINITE WHILE LOOP """
+
+
+
--- pid.py
+++ pid.py
@@ -1,8 +1,8 @@
 import time
 
+
 # Creates class object for the PID controller
 class PID:
-
     """
         Parameters:
             @param kproportional = proportional gain
@@ -13,15 +13,16 @@ class PID:
 
             @return => void function
     """
-    def __init__(self, kproportional, kintegral, kderivative, maximum_integral, maximum_output):
+
+    def __init__(self, kproportional, kintegral, kderivative, maximum_integral, minimum_output, maximum_output):
         self.kp = kproportional
         self.ki = kintegral
         self.kd = kderivative
         self.integral = 0
         self.max_integral = maximum_integral
-        self.min_out = -100
+        self.min_out = minimum_output  # Below this and motors won't spin
         self.max_out = maximum_output
-        self.past_positions = [0, 0] # Stores past positions of the object for derivative calculations
+        self.past_positions = [0, 0]  # Stores past positions of the object for derivative calculations
         self.measurement_time = [time.time(), 0]
         # print("PID Controller Constructed") # Debugging purposes
 
@@ -32,10 +33,12 @@ class PID:
 
             @return = PWM value for motors (motor power)
     """
+
     def get_output(self, current, desired):
         # print("Getting output") # Debugging purposes
+        print("INput vlaues: ", str(current), " ", str(desired))
 
-        error = desired - current
+        error = abs(desired - current)
 
         # Determines derivative value
         self.past_positions[1] = self.past_positions[0]
@@ -58,7 +61,7 @@ class PID:
 
         # If error is less than a specified amount, then integral value is dropped to reduce speed
         if abs(error) < 10 and error != 0:
-            self.integral = round(self.integral/4)
+            self.integral = round(self.integral / 4)
         # If error is zero, output is set to zero
         elif error == 0:
             self.integral = 0
@@ -66,17 +69,19 @@ class PID:
 
         # Calculates required motor output and ensures it stays within min and max values
         motor_output = round(error * self.kp + self.integral * self.ki + derivative * self.kd)
+        print("Raw value: ", str(motor_output))
 
         if motor_output < self.min_out:
             motor_output = self.min_out
         elif motor_output > self.max_out:
             motor_output = self.max_out
 
-        # print("Values: ", str(error), "; ", str(self.integral), "; ", str(derivative))
-        # print("Output: ", str(motor_output))
-        # print()
+        print("Values: ", str(error), "; ", str(self.integral), "; ", str(derivative))
+        print("Output: ", str(motor_output))
+        print()
 
-        return motor_output # Returns the required PWM value
+        return motor_output  # Returns the required PWM value
+
 
 
 
